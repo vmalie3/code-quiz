a7c8c9278d1258e559367ce3db7e3d678911ac0b
Victoria Malie <vmalie3@gmail.com>
2022-05-26 14:45:51 -0500

    created a timer

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ var quizEl = document.querySelector('#quizBody');
 var endEl = document.querySelector('#end');
 var beginBtn = document.querySelector('#begin');
 var quizTitle = document.querySelector('#quizBody #title');
+var timer = document.querySelector('#timeLeft')
 
-var questions = [
+var questionsArr = [
     {
         question: 'What is the typical color of grass?',
         choices: ['red', 'orange', 'purple', 'green'],
@@ -19,42 +20,67 @@ var questions = [
     },
     {
         question: 'Where is Fort Lauderdale located?',
-        choices: ['Florida','China', 'New York', 'Oklahoma'],
+        choices: ['Florida', 'China', 'New York', 'Oklahoma'],
         answer: 'Florida'
     }
 
 ]
 
 function displayState() {
-  if (state === 'begin') {
-    startEl.style.display = 'block';
-    quizEl.style.display = 'none';
-    endEl.style.display = 'none';
-  }
-  if (state === 'quizBody') {
-    startEl.style.display = 'none';
-    quizEl.style.display = 'block';
-    endEl.style.display = 'none';
-  }
-  if (state === 'end') {
-    startEl.style.display = 'none';
-    quizEl.style.display = 'none';
-    endEl.style.display = 'block';
-  }
+    if (state === 'begin') {
+        startEl.style.display = 'block';
+        quizEl.style.display = 'none';
+        endEl.style.display = 'none';
+    }
+    if (state === 'quizBody') {
+        startEl.style.display = 'none';
+        quizEl.style.display = 'block';
+        endEl.style.display = 'none';
+    }
+    if (state === 'end') {
+        startEl.style.display = 'none';
+        quizEl.style.display = 'none';
+        endEl.style.display = 'block';
+    }
 }
 
 function init() {
-  displayState();
+    displayState();
 }
 
-beginBtn.addEventListener("click", function() {
-  state = 'quizBody';
-  displayState();
+function displayTimer() {
+    timer.textContent = time;
+}
+
+var time = 24;
+
+function setTimer() {
+    var timerInterval = setInterval(function () {
+        time--;
+        displayTimer();
+
+        if (time === 0) {
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+
+function displayQuestions() {
+    
+}
+
+beginBtn.addEventListener("click", function () {
+    state = 'quizBody';
+    displayState();
+    setTimer();
+
 });
 
+
 quizTitle.addEventListener("click", function () {
-  state = 'end';
-  displayState();
+    state = 'end';
+    displayState();
 });
 
 init();
